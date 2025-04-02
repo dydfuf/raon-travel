d93c909ba35d88b8b4e2a50f856ca3226f15642c
dydfuf <88dydfuf@naver.com>
2025-04-02 21:34:56 +0900

    채팅 기능을 구현합니다.

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,135 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import {
+  ChatBubbleAction,
+  ChatBubbleAvatar,
+  ChatBubbleMessage,
+} from "@/src/components/ui/chat/chat-bubble";
+import { ChatMessageList } from "@/src/components/ui/chat/chat-message-list";
+import { ChatBubble } from "@/src/components/ui/chat/chat-bubble";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { CoreMessage } from "ai";
+import { useState } from "react";
+import {
+  CopyIcon,
+  CornerDownLeft,
+  Mic,
+  Paperclip,
+  RefreshCcw,
+  Send,
+  Volume2,
+} from "lucide-react";
+import { ChatInput } from "@/src/components/ui/chat/chat-input";
+import { Button } from "@/components/ui/button";
+
+const ChatAiIcons = [
+  {
+    icon: CopyIcon,
+    label: "Copy",
+  },
+  {
+    icon: RefreshCcw,
+    label: "Refresh",
+  },
+  {
+    icon: Volume2,
+    label: "Volume",
+  },
+];
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [isGenerating, setIsGenerating] = useState(false);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState<CoreMessage[]>([]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  return (
+    <main>
+      <div>
+        <ChatInput
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+          onKeyDown={async (event) => {
+            if (event.key === "Enter") {
+              setMessages((currentMessages) => [
+                ...currentMessages,
+                { role: "user", content: input },
+              ]);
+
+              const response = await fetch("/api/chat", {
+                method: "POST",
+                body: JSON.stringify({
+                  messages: [...messages, { role: "user", content: input }],
+                }),
+              });
+
+              const { messages: newMessages } = await response.json();
+
+              setMessages((currentMessages) => [
+                ...currentMessages,
+                ...newMessages,
+              ]);
+            }
+          }}
+        />
+
+        <ChatMessageList>
+          {messages.map((message, index) => (
+            <ChatBubble
+              key={`${message.role}-${index}`}
+              variant={message.role == "user" ? "sent" : "received"}
+            >
+              <ChatBubbleAvatar
+                src=""
+                fallback={message.role == "user" ? "👨🏽" : "🤖"}
+              />
+
+              <ChatBubbleMessage>
+                {messages.map((message, index) => (
+                  <div key={`${message.role}-${index}`}>
+                    {typeof message.content === "string"
+                      ? message.content
+                      : message.content
+                          .filter((part) => part.type === "text")
+                          .map((part, partIndex) => (
+                            <Markdown
+                              key={partIndex}
+                              remarkPlugins={[remarkGfm]}
+                            >
+                              {part.text}
+                            </Markdown>
+                          ))}
+                  </div>
+                ))}
+
+                {message.role === "assistant" &&
+                  messages.length - 1 === index && (
+                    <div className="flex items-center mt-1.5 gap-1">
+                      {!isGenerating && (
+                        <>
+                          {ChatAiIcons.map((icon, iconIndex) => {
+                            const Icon = icon.icon;
+                            return (
+                              <ChatBubbleAction
+                                variant="outline"
+                                className="size-5"
+                                key={iconIndex}
+                                icon={<Icon className="size-3" />}
+                                onClick={() => {}}
+                              />
+                            );
+                          })}
+                        </>
+                      )}
+                    </div>
+                  )}
+              </ChatBubbleMessage>
+            </ChatBubble>
+          ))}
+        </ChatMessageList>
+      </div>
+    </main>
   );
 }
