a58f35ab6757ec29da43390d9aee54e2d8cf61b8
dydfuf <88dydfuf@naver.com>
2025-04-10 00:50:43 +0900

    메타데이터를 추가합니다.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1"
+      />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
